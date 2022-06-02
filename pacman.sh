a8e8e99831f375f38e4b4248d7e2cd6286dc7ff3
wusung <twkoci@gmail.com>
2022-06-02 11:01:08 +0800

    Update pacman.sh

--- pacman.sh
+++ pacman.sh
@@ -4,11 +4,11 @@ pacman -Syu base linux linux-firmware \
  amd-ucode networkmanager iw wpa_supplicant \
  grub efibootmgr \
  bluez pulseaudio-alsa pulseaudio-bluetooth \
- mesa xf86-video-amdgpu libva-mesa-driver mesa-vdpau \ # AMD GPU
+ mesa xf86-video-amdgpu libva-mesa-driver mesa-vdpau \
  firefox microsoft-edge-stable-bin \
  ttf-freefont ttf-roboto noto-fonts noto-fonts-emoji noto-fonts-cjk adobe-source-code-pro-fonts \
  sudo vim git base-devel sddm \
- gdm gnome gnome-terminal # GNOME
+ gdm gnome gnome-terminal
 
 # os-prober ntfs-3g \
  # mesa xf86-video-intel intel-media-driver libva-intel-driver \ # Intel GPU
