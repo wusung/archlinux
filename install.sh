e47748e9b4eeadcef00de634735beae75a9b814b
wusung <twkoci@gmail.com>
2022-06-02 09:01:51 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,1 @@
+pacstrap /mnt base linux linux-firmware intel-ucode networkmanager iw wpa_supplicant grub efibootmgr os-prober ntfs-3g bluez pulseaudio-alsa pulseaudio-bluetooth mesa xf86-video-intel intel-media-driver libva-intel-driver sddm plasma konsole chromium firefox ttf-freefont ttf-roboto noto-fonts noto-fonts-emoji noto-fonts-cjk adobe-source-code-pro-fonts sudo vim git base-devel
