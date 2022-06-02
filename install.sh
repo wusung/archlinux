10ff7414470372efefe20db327239b97ab044109
wusung <twkoci@gmail.com>
2022-06-02 10:13:08 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 # pacstrap /mnt base linux linux-firmware intel-ucode networkmanager iw wpa_supplicant grub efibootmgr os-prober ntfs-3g bluez pulseaudio-alsa pulseaudio-bluetooth mesa xf86-video-intel intel-media-driver libva-intel-driver sddm plasma konsole chromium firefox ttf-freefont ttf-roboto noto-fonts noto-fonts-emoji noto-fonts-cjk adobe-source-code-pro-fonts sudo vim git base-devel
 
 pacstrap /mnt base linux linux-firmware \
- intel-ucode networkmanager iw wpa_supplicant \
+ amd-ucode networkmanager iw wpa_supplicant \
  grub efibootmgr os-prober ntfs-3g bluez pulseaudio-alsa \
  pulseaudio-bluetooth mesa xf86-video-intel intel-media-driver \
  libva-intel-driver chromium firefox ttf-freefont \
