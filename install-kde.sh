7447d18ebd01f2f7494c5db859f933caea00e4e4
wusung <twkoci@gmail.com>
2022-06-07 07:04:07 +0800

    Update install-kde.sh

--- install-kde.sh
+++ install-kde.sh
@@ -14,17 +14,20 @@ pacman -Syu --noconfirm firefox \
 
 pacman -Syu --noconfirm ttf-freefont \
  ttf-roboto noto-fonts noto-fonts-emoji noto-fonts-cjk adobe-source-code-pro-fonts community/ttf-jetbrains-mono
- 
 
+# KDE
 pacman -Syu --noconfirm latte-dock packagekit-qt5 
-
 
 # SuperKey - Add Alt-F1 to shortcut
 kwriteconfig5 --file ~/.config/kwinrc --group ModifierOnlyShortcuts --key Meta "org.kde.lattedock,/Latte,org.kde.LatteDock,activateLauncherMenu"
 qdbus org.kde.KWin /KWin reconfigure
 
-
 # flutter development
 pacman -Syu --noconfirm clang cmake ninja-build pkg-config gtk3 xz unzip
+
+# aurman
+git clone https://aur.archlinux.org/aurman.git
+cd aurman
+makepkg -si
 
 aurman -Syu google-chrome
