dba850203808de679ba66395063347fe3ea44ec0
wusung <twkoci@gmail.com>
2022-06-06 08:33:26 +0800

    Create install-kde.sh

---
+++ install-kde.sh
@@ -0,0 +1,12 @@
+# Driver
+#pacman -Syu base linux linux-firmware \
+# base linux linux-firmware \
+# amd-ucode networkmanager iw wpa_supplicant \
+# grub efibootmgr os-prober ntfs-3g bluez pulseaudio-alsa \
+# pulseaudio-bluetooth mesa xf86-video-intel intel-media-driver \
+# libva-intel-drive
+ 
+# Utilities
+pacman -Syu r firefox ttf-freefont \
+ ttf-roboto noto-fonts noto-fonts-emoji noto-fonts-cjk adobe-source-code-pro-fonts \
+ sudo vim git base-devel
