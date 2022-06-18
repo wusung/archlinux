8315ee96236b61356fe21c1045182ca9cc879f69
wusung <twkoci@gmail.com>
2022-06-18 07:17:37 -0700

    Update install-devel.sh

--- install-devel.sh
+++ install-devel.sh
@@ -4,6 +4,6 @@ build/install-build-deps.sh --no-chromeos-font
 
 export PKG_CONFIG_PATH=/usr/lib/x86_64-linux-gnu/pkgconfig:/usr/share/pkgconfig:/usr/lib/pkgconfig
 
-apt install -y libdbus-glib-1-dev ninja-build clang
+apt install -y libdbus-glib-1-dev ninja-build clang libpipewire-0.3-dev libgnome-keyring-dev
 
 build/linux/sysroot_scripts/install-sysroot.py --arch=amd64
