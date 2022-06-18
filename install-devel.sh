67a654cdfd2ad183cf87dfef87c367c7056a53ff
wusung <twkoci@gmail.com>
2022-06-18 13:35:05 +0800

    Update install-devel.sh

--- install-devel.sh
+++ install-devel.sh
@@ -5,3 +5,5 @@ build/install-build-deps.sh --no-chromeos-font
 export PKG_CONFIG_PATH=/usr/lib/x86_64-linux-gnu/pkgconfig
 
 apt install -y libdbus-glib-1-dev ninja-build clang
+
+build/linux/sysroot_scripts/install-sysroot.py --arch=amd64
