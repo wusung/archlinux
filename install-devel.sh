0b2b48fcfba4ca85f67217b7b20444b0e98d97c5
wusung <twkoci@gmail.com>
2022-06-18 13:32:24 +0800

    Update install-devel.sh

--- install-devel.sh
+++ install-devel.sh
@@ -6,4 +6,4 @@ build/install-build-deps.sh --no-chromeos-font
 
 export PKG_CONFIG_PATH=/usr/lib/x86_64-linux-gnu/pkgconfig
 
-apt install -y libdbus-glib-1-dev
+apt install -y libdbus-glib-1-dev ninja-build clang
