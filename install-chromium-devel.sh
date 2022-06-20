730190386476bfc148f09f78cc3857f4d7ef4915
wusung <twkoci@gmail.com>
2022-06-20 10:53:49 +0800

    Update install-chromium-devel.sh

--- install-chromium-devel.sh
+++ install-chromium-devel.sh
@@ -6,6 +6,12 @@ export PKG_CONFIG_PATH=/usr/lib/x86_64-linux-gnu/pkgconfig:/usr/share/pkgconfig:
 
 apt install -y libdbus-glib-1-dev ninja-build clang libpipewire-0.3-dev wget
 
-# wget https://download.jetbrains.com/toolbox/jetbrains-toolbox-1.24.12080.tar.gz
+# Install Jetbrains Toolbox
+pushd /tmp
+wget https://download.jetbrains.com/toolbox/jetbrains-toolbox-1.24.12080.tar.gz
+tar zxvf jetbrains-toolbox-1.24.12080.tar.gz
+mkdir ~/.local/jetbrains-toolbox
+mv /tmp/jetbrains-toolbox-1.24.12080/jetbrains-toolbox ~/.local/jetbrains-toolbox
+pushd
 
 build/linux/sysroot_scripts/install-sysroot.py --arch=amd64
