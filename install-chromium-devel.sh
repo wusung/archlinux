20908f6806683bbeb21a98263b533cc0ffbfda4b
wusung <twkoci@gmail.com>
2022-06-22 14:16:22 +0800

    Update install-chromium-devel.sh

--- install-chromium-devel.sh
+++ install-chromium-devel.sh
@@ -1,5 +1,9 @@
 # Chromium development
 
+mount -t fuse.vmhgfs-fuse -o allow_other,uid=1000,gid=1000 .host:/Projects /home/debian/Projects
+
+
+
 build/install-build-deps.sh --no-chromeos-fonts
 
 export PKG_CONFIG_PATH=/usr/lib/x86_64-linux-gnu/pkgconfig:/usr/share/pkgconfig:/usr/lib/pkgconfig
